1c820b771b732b61585d9192c0f8625209b89f7e
vkosachev <kosachevlad@gmail.com>
2022-01-24 22:35:27 +0200

    extracted function amountFor

--- index.js
+++ index.js
@@ -35,37 +35,15 @@ function statement(invoice, plays) {
   
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0
+    let thisAmount = amountFor(perf.play);
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-
-      case "comedy":
-        thisAmount = 30000
-
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
-        }
-        thisAmount += 300 * perf.audience
-        break
-
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
-
-    // bonuses adding
+    // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
 
-    // additional bonus on every 10 comedies
+    // add extra credit for every ten comedy attendees
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
 
-    // Output summary string
+    // Print line for this string
     result += ` ${play.name}: ${format(thisAmount / 100)}`
     result += ` (${perf.audience} seats) \n`
     totalAmount += thisAmount
@@ -74,6 +52,32 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`
   console.log(result)
   return result
+}
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30)
+      }
+      break
+
+    case "comedy":
+      thisAmount = 30000
+
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20)
+      }
+      thisAmount += 300 * perf.audience
+      break
+
+    default:
+      throw new Error(`unknown type: ${play.type}`)
+  }
+  return thisAmount;
 }
 
 statement(invoices, plays)
