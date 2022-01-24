7d1ef59af6bb5bf8e5bb8b58c9760712a5a872eb
vkosachev <kosachevlad@gmail.com>
2022-01-24 23:04:26 +0200

    applied inline variable amountFor(perf)

--- index.js
+++ index.js
@@ -34,8 +34,6 @@ function statement(invoice, plays) {
     }).format
   
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
 
@@ -43,9 +41,9 @@ function statement(invoice, plays) {
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
 
     // Print line for this string
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)}`
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)}`
     result += ` (${perf.audience} seats) \n`
-    totalAmount += thisAmount
+    totalAmount += amountFor(perf)
   }
   result += `Amount owed id ${format(totalAmount/100)}\n`
   result += `You earned ${volumeCredits} credits\n`
