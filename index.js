949e1242e1aecbf05264b4526d58879df5154534
vkosachev <kosachevlad@gmail.com>
2022-01-25 23:16:30 +0200

    extracting volumeCredits

--- index.js
+++ index.js
@@ -34,20 +34,24 @@ function statement(invoice, plays) {
     }).format
   
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0)
-
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
-
+    volumeCredits += volumeCreditsFor(perf)
+    
     // Print line for this string
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)}`
-    result += ` (${perf.audience} seats) \n`
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)}  (${perf.audience} seats) \n`
     totalAmount += amountFor(perf)
   }
   result += `Amount owed id ${format(totalAmount/100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   console.log(result)
+  return result
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0
+  result += Math.max(aPerformance.audience - 30, 0)
+
+  // add extra credit for every ten comedy attendees
+  if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
   return result
 }
 
