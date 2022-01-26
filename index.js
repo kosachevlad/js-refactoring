d475bffe16a92162d1962f77bc6cafda95ee4c4d
vkosachev <kosachevlad@gmail.com>
2022-01-26 22:24:24 +0200

    removed the format variable to usd function

--- index.js
+++ index.js
@@ -27,23 +27,26 @@ function statement(invoice, plays) {
   let volumeCredits = 0
 
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format
   
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf)
-    
+
     // Print line for this string
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)}  (${perf.audience} seats) \n`
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)}  (${perf.audience} seats) \n`
     totalAmount += amountFor(perf)
   }
-  result += `Amount owed id ${format(totalAmount/100)}\n`
+  result += `Amount owed id ${usd(totalAmount/100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   console.log(result)
   return result
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 2
+        }).format(aNumber)
 }
 
 function volumeCreditsFor(aPerformance) {
